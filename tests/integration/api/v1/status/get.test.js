7fdbfea68be6420e3119d6082491f51318a7e923
Alberico Bezerra <alberico.sp@gmail.com>
2025-01-15 17:57:41 -0300

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,17 +4,21 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBady = await response.json();
-  expect(responseBady.updated_at).toBeDefined();
+      const responseBady = await response.json();
+      expect(responseBady.updated_at).toBeDefined();
 
-  const parseUpdatedAt = new Date(responseBady.updated_at).toISOString();
-  expect(responseBady.updated_at).toEqual(parseUpdatedAt);
+      const parseUpdatedAt = new Date(responseBady.updated_at).toISOString();
+      expect(responseBady.updated_at).toEqual(parseUpdatedAt);
 
-  expect(responseBady.dependencies.database.version).toEqual("16.0");
-  expect(responseBady.dependencies.database.max_connections).toEqual(100);
-  expect(responseBady.dependencies.database.opened_connections).toEqual(1);
+      expect(responseBady.dependencies.database.version).toEqual("16.0");
+      expect(responseBady.dependencies.database.max_connections).toEqual(100);
+      expect(responseBady.dependencies.database.opened_connections).toEqual(1);
+    });
+  });
 });
